00d879fa07f87823344d135eafcf9bff4593cab4
reihera <nightshadegamer200yt@gmail.com>
2024-12-16 12:31:50 -0500

    made it work

--- BYL.js
+++ BYL.js
@@ -1,5 +1,5 @@
 document.body.textContent = "";
 
 let header = document.createElement("h1");
-header.textContent = "This page has been eaten";
+header.textContent = "gay porn";
 document.body.appendChild(header);
