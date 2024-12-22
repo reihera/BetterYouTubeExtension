204f6512ec0ec1353613de4178858fe2ce1543d9
reihera <nightshadegamer200yt@gmail.com>
2024-12-22 16:59:10 -0500

    im so hungry

--- inject.js
+++
@@ -1,5 +0,0 @@
-//let text = "Shorts";
-//let result = text.replace("Shorts", "Explore");
-console.log("it shoulda replaced shorts god dammit i fucking hate javascript man");
->Shorts<.replaceWith(>Explore<);
-console.error("kill yourelf");
