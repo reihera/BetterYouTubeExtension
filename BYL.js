f10a341bc9500272cb3195091ac4b5adedbffa28
reihera <nightshadegamer200yt@gmail.com>
2025-01-07 13:42:14 -0500

    cleaned up channel page, made version number correct, commented out uselss js

---
+++ BYL.js
@@ -0,0 +1,21 @@
+/*function change(iconID) {
+    var element = document.getElementById(iconID);
+
+    if (element.classList.contains("M20 12h-8v8h-1v-8H3v-1h8V3h1v8h8v1z")) {
+
+        element.classList.add("keyboard_arrow_down")
+
+    } else {
+
+        element.classList.add("keyboard_arrow_up")
+    }
+}*/
+/*function MoveExplore() {
+    var explore = document.createDocumentFragment();
+    explore.appendChild(document.getElementById('yt-simple-endpoint style-scope ytd-guide-entry-renderer'));
+    document.getElementsByClassName('guide-section-title').appendChild(explore);
+    console.log("i may kill myself");
+}
+MoveExplore()
+*/
+console.log("congratulations, you have loaded a useless js file.")
